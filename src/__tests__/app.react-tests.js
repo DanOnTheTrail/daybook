c80cd69a2063a4e6612a480c63371720a14460a5
Dan <5843830+DanOnTheTrail@users.noreply.github.com>
2021-08-22 15:38:14 -0400

    fix linting issues

--- src/__tests__/app.react-tests.js
+++ src/__tests__/app.react-tests.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {it, expect} from 'jest';
 import { Hello } from '../hello';
 
 it('renders correctly', () => {
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Hello = () => {
+export const Hello = (): JSX.Element => {
   return (
     <h2>Hello from React!</h2>
     );
